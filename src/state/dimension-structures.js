25b4f4bb95290e6ea73414fc22b27d80070f7ad9
oleg-erom <oleg@eromagency.com>
2022-09-19 21:53:36 +0300

    fixed: nested droppables not accepting draggables
    https://github.com/atlassian/react-beautiful-dnd/issues/2385#issuecomment-1251003147

--- src/state/dimension-structures.js
+++ src/state/dimension-structures.js
@@ -10,10 +10,18 @@ import type {
 
 export const toDroppableMap = memoizeOne(
   (droppables: DroppableDimension[]): DroppableDimensionMap =>
-    droppables.reduce((previous, current) => {
-      previous[current.descriptor.id] = current;
-      return previous;
-    }, {}),
+    droppables
+      // Sort by nested level - deepest levels must be first
+      // fixes https://github.com/atlassian/react-beautiful-dnd/issues/2385#issuecomment-1251003147
+      .sort(
+        (
+          { page: { contentBox: { left: a } } },
+          { page: { contentBox: { left: b } } }
+        ) => (
+          a === b ? 0 : (a > b ? -1 : 1)
+        )
+      )
+      .reduce((previous, current) => ({ ...previous, [current.descriptor.id]: current }), {}),
 );
 
 export const toDraggableMap = memoizeOne(
